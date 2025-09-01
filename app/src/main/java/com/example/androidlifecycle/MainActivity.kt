3cc01d907dc78261f099a68c395cff1f5334aea0
Monang Gultom <94420546+MonangGultom-Xacti@users.noreply.github.com>
2025-09-01 11:51:29 +0900

    #188329 動画分割時に動画ファイル名が異常になる
    [内容]
    -バグ修正1
    
    [指摘元]
    設計指摘不具合修正
    
    [影響範囲]
    分割された動画ファイル名
    
    [備考]
    -

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
 
-        Log.d(TAG, "MONANG:: Testing Branch onStart Commit1")
+        Log.d(TAG, "MONANG:: Testing Branch onStart Commit2")
 
         lifecycleTextView = findViewById(R.id.lifecycleTextView)
         restartButton = findViewById(R.id.restartButton)
