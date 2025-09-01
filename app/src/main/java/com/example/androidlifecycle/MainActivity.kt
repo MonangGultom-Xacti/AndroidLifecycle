440145e8b6973bfa3aac5e8fc77c9f67157c345c
Monang Gultom <94420546+MonangGultom-Xacti@users.noreply.github.com>
2025-09-01 11:37:22 +0900

    #188326 AIモザイクONの時に解像度の変更が反映されない
    [内容]
    -
    
    [指摘元]
    設計指摘不具合修正
    
    [影響範囲]
    ・AIモザイク動作
    ・アプリ設定画面からライブビュー画面への遷移
    ・カメラ設定画面からライブビュー画面への遷移
    
    [備考]
    -

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
 
-        Log.d(TAG, "MONANG:: Testing Branch onPause Commit3")
+        Log.d(TAG, "MONANG:: Testing Branch onStop Commit1")
 
         lifecycleTextView = findViewById(R.id.lifecycleTextView)
         restartButton = findViewById(R.id.restartButton)
